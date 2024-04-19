581fd498a69598d9edd229adae833a6f48425436
KARTIK SHARMA <kartiksharmamrt1@gmail.com>
2024-04-19 17:37:32 +0530

    change 5

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var app = express();
-app.use(express.json());
+
 app.set("view engine", "ejs");
-const about = require('./views/about.ejs');
 
 app.get("/", function(req,res){
-	res.send('home');
+	res.render("home");
 });
 
-app.get("/about", about);
+app.get("/about", function (req,res){
+	res.render("about");
+});
 
-app.listen(8080, () => {
-    console.log('Server is listenin on PORT :' + PORT);
-})
+app.listen(3000,function(){
+	console.log(" App just started ");
+});
