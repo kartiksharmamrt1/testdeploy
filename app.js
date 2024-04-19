01fa8fa1c6e26e90d8f000eda88e825d2f5a6460
KARTIK SHARMA <kartiksharmamrt1@gmail.com>
2024-04-19 17:25:03 +0530

    change 3

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require("express");
 var app = express();
 app.use(express.json());
 app.set("view engine", "ejs");
-const about = require('./views/about');
+const about = require('./views/about.ejs');
 
 app.get("/", function(req,res){
 	res.send('home');
 });
 
-app.use("/about", about);
+app.get("/about", about);
 
 app.listen(process.env.PORT,process.env.IP);
